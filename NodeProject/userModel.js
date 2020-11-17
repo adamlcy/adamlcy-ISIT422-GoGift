11cf115361095955c5edc2eef828501c91ab2d64
Thaisap <tha.apda@gmail.com>
2020-11-17 15:28:33 -0800

    usermodel uncommented

--- NodeProject/userModel.js
+++ NodeProject/userModel.js
@@ -1,4 +1,4 @@
-/*  const mongoose = require('mongoose');
+ const mongoose = require('mongoose');
 const tag = require('./tagModel');
 const item = require('./itemModel');
 const bcrypt = require('bcrypt') 
@@ -22,10 +22,6 @@ const user = new mongoose.Schema({
     type: String
   }, 
 
-   email: { type: String, require: true },
-    username: { type: String, require: true },
-    password: { type: String, require: true },
-    creation_dt: { type: Date, require: true },
 
   tag: [{type: mongoose.Schema.Types.ObjectId, ref: 'Tag'}],
 
@@ -35,19 +31,12 @@ const user = new mongoose.Schema({
  
 }, {"collection":"user"});
 
-schema.statics.hashPassword = function hashPassword(password) {
-    return bcrypt.hashSync(password, 10);
-}
-
-schema.methods.isValid = function (hashedpassword) {
-    return bcrypt.compareSync(hashedpassword, this.password);
-}
 
 module.exports = mongoose.model('User', schema);
 
-/*
+
 const User = mongoose.model("User", user);
 module.exports = User;
 
- */
+
  
