5ba5abeae8a769936625bc21205b2dd9c70e7d29
Thaisap <tha.apda@gmail.com>
2020-11-23 17:22:39 -0800

    usercredentials

--- NodeProject/models/usercredential.js
+++ NodeProject/models/usercredential.js
@@ -1,13 +1,17 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt');
+const usermodel = require('../userModel')
 
 var schema = new Schema({
     email: { type: String, require: true },
     username: { type: String, require: true },
     password: { type: String, require: true },
-    creation_dt: { type: Date, require: true }
-});
+    creation_dt: { type: Date, require: true },
+    gogift: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+
+}, {"collection":"userCredentials"});
+
 
 schema.statics.hashPassword = function hashPassword(password) {
     return bcrypt.hashSync(password, 10);
@@ -16,5 +20,6 @@ schema.statics.hashPassword = function hashPassword(password) {
 schema.methods.isValid = function (hashedpassword) {
     return bcrypt.compareSync(hashedpassword, this.password);
 }
+
 
 module.exports = mongoose.model('users', schema);
